60a3762bc69a86f0504f9d94a983a7e86d6845c9
prkkhan786 <prkkhan1@gmail.com>
2022-01-17 15:33:58 +0530

    change

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-import express from 'express';
-import fs from 'fs';
-import path from 'path';
+import express from "express";
+import fs from "fs";
+import path from "path";
 
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
+import React from "react";
+import ReactDOMServer from "react-dom/server";
 
-import App from '../src/App';
+import App from "../src/App";
 
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use('^/$', (req, res, next) => {
-  fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
+app.use("^/$", (req, res, next) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
     if (err) {
       console.log(err);
-      return res.status(500).send('Some error happened');
+      return res.status(500).send("Some error happened");
     }
     return res.send(
       data.replace(
@@ -26,7 +26,7 @@ app.use('^/$', (req, res, next) => {
   });
 });
 
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(express.static(path.resolve(__dirname, "..", "build")));
 
 app.listen(PORT, () => {
   console.log(`App launched on ${PORT}`);
